4a867edd300ad0042d879026ad8a568220b149fe
Daniel Succar <danielsuccar@icloud.com>
2019-12-29 23:23:47 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var katzDeli = []
+
+function takeANumber(deliLine,Name){
+  deliLine.push(`${Name}`)
+  return(`Welcome, ${Name}. You are number ${deliLine.length} in line.`)
+}
+
+function nowServing (deliLine){
+  let i = 0
+  while(i < deliLine.length){
+    i++
+  }
+  if (deliLine >=1) {
+    return (`currently serving ${deliLine.shift()`
+  }
+  else {
+    return ("There is nobody waiting to be served!")
+  }
+}
