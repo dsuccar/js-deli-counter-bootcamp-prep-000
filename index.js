04698912e0940499560e30f39e9565c087e6d9ca
Daniel Succar <danielsuccar@icloud.com>
2019-12-29 23:53:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function currentLine(line){
   return `The line is currently empty.`;
   else if (line.length > 0) {
     return (`The line is currently: 1. Bill, 2. Jane, 3. Ann`)
+  }
+}
 
 
   }{
