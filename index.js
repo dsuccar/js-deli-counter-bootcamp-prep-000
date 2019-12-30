caf3411e98f7bb64fb9e68ebc87ee9b97f216104
Daniel Succar <danielsuccar@icloud.com>
2019-12-30 00:15:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeli) {
 function currentLine(line){
   if (line.length === 0)
   return `The line is currently empty.`;
-  else if (line.length > 0) {
-    return (`The line is currently: ${index }` + (line.length) + line)
-  }
-}
+  for (var i = 0; i < line.length; i++) {
+       lineNum.push(` ${i+1}. ${line[i]}`);
+   	}
+     return "The line is currently:" + lineNum;
